0e08216a389a6cdc39daad9533406b9d1639da88
imDev2023 <141978959+imDev2023@users.noreply.github.com>
2023-09-07 02:54:37 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let someNumer = 33
+let stringNumber = String(someNumer)
+console.log(stringNumber);
+console.log(typeof stringNumber);
