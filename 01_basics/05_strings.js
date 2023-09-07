b1c3433d7e1e6ec2e32403946b0f600ef289450a
imDev2023 <141978959+imDev2023@users.noreply.github.com>
2023-09-07 17:51:03 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "Farhan"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); (Outdated way)
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Farhan");
+
+console.log(gameName.bold);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.substring());
