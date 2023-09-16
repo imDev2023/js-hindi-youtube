6b5ce2e8f78d235843d2ff1c1705b21f6d153252
imDev2023 <141978959+imDev2023@users.noreply.github.com>
2023-09-16 18:04:43 +0000

    whileloop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,23 @@
+// while && do-while 
+
+// let index = 0;
+// while(index <=10){
+//     console.log(`Value of index is ${index}`);
+//     index = index+2;
+// }
+
+let myArray = ["one", "Two", "Three"]
+
+let i = 0;
+
+while (i < myArray.length){
+    // console.log(`Value of myArray is ${myArray[i]}`)
+    i = i + 1;
+}
+
+let score = 1;
+
+do{
+    console.log(`score is ${score}`);
+    score++;
+}while (score <=10);
