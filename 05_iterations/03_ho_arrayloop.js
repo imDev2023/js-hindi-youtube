31567d386c78c0a948333b5aca9af085f6f58b1d
imDev2023 <141978959+imDev2023@users.noreply.github.com>
2023-09-16 18:33:16 +0000

    hoarrayloops

---
+++ 05_iterations/03_ho_arrayloop.js
@@ -0,0 +1,32 @@
+// for of
+
+const arr = [1,2,3,4,5];
+
+for (const num of arr) {
+    // console.log(num);
+    if( num == 3){
+        continue;
+    }
+}
+
+const greetings = "Hello Worlds";
+
+for (const greet of greetings) {
+    // console.log(`lets see the value of ${greet}`);
+}
+
+// Maps
+
+const map = new Map()
+map.set ("PK","Pakistan");
+map.set ("IN","India");
+map.set ("SR","Sri Lanka");
+map.set ("UK", "United Kingdom");
+
+// console.log(map);
+
+for (const [key, value] of map) {
+
+    console.log(`${key} :- ${value}`);
+    
+}
