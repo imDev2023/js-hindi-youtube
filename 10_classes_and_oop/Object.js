e0bb67206360c6490926086934244a34dc01c301
imDev2023 <141978959+imDev2023@users.noreply.github.com>
2023-10-03 01:09:10 +0000

    Objects

--- 10_classes_and_oop/Object.js
+++ 10_classes_and_oop/Object.js
@@ -1,32 +1,31 @@
 function multipleBy5(num){
-
-    return num*5
+    return num * 5;
 }
-
 multipleBy5.power = 2
 
-console.log(multipleBy5(5));
+console.log(multipleBy5(2))
 console.log(multipleBy5.power);
-console.log(multipleBy5.prototype);
+console.log(multipleBy5.prototype)
 
 function createUser(username, score){
-    this.username = username
-    this.score = score
+    this.username=username;
+    this.score = score;
 }
 
 createUser.prototype.increment = function(){
     this.score++
 }
+
 createUser.prototype.printMe = function(){
     console.log(`price is ${this.score}`);
 }
 
+
 const chai = new createUser("chai", 25)
-const tea = createUser("tea", 250)
+const tea = new createUser("tea", 250)
 
 chai.printMe()
-
-
+tea.printMe()
 /*
 
 Here's what happens behind the scenes when the new keyword is used:
