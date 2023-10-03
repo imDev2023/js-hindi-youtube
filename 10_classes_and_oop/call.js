a140d384a71f99187fe5b6c72e72d0087dc8d86a
imDev2023 <141978959+imDev2023@users.noreply.github.com>
2023-10-03 02:05:45 +0000

    call

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -1,15 +1,14 @@
-function SetUsername (username){
+function SetUsername(username){
     // complex DB Calls
-    this.username = username
+    this.username = username;
 }
 
 function createUser(username, email, password){
     SetUsername.call(this, username)
-
     this.email = email
-    this.password= password
+    this.password = password
 }
 
-const chai = new createUser("chair", "chair@fb.com", "1234")
+const chai = new createUser("Farhan", "chai@fb.com", "123")
 
 console.log(chai);
